e348624070a7bf05d27789a434faad34df8ecd8c
Marcin Chalczynski <m.chalczynski@gmail.com>
2020-07-16 20:07:30 +0200

    config: service configuration
    changelog: none
    
    Signed-off-by: Marcin Chalczynski <m.chalczynski@gmail.com>

---
+++ config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"github.com/mendersoftware/go-lib-micro/config"
+)
+
+const (
+	// SettingListen is the config key for the listen port
+	SettingListen        = "listen"
+	SettingListenDefault = "8080"
+
+	// SettingMenderBackend is the config key for the Mender base url (scheme + host:port)
+	SettingMenderBackend        = "mender_backend"
+	SettingMenderBackendDefault = ""
+
+	// SettingMenderUser is the Ambassador's Mender login
+	SettingMenderUser        = "mender_user"
+	SettingMenderUserDefault = ""
+
+	// SettingMenderPass is the Ambassador's Mender login
+	SettingMenderPass        = "mender_pass"
+	SettingMenderPassDefault = ""
+
+	// SettingServerCert is the Ambassador's https server cert
+	SettingServerCert        = "server_cert"
+	SettingServerCertDefault = "/etc/mtls/certs/server/server.crt"
+
+	// SettingServerKey is the Ambassador's https server key
+	SettingServerKey        = "server_key"
+	SettingServerKeyDefault = "/etc/mtls/certs/server/server.key"
+
+	// SettingTenantCAPem is the tenant's CA cert used to verify client mTLS certs
+	SettingTenantCAPem        = "tenant_ca_pem"
+	SettingTenantCAPemDefault = "/etc/mtls/certs/tenant-ca/tenant.ca.pem"
+
+	// SettingDebugLog is the config key for the turning on the debug log
+	SettingDebugLog        = "debug_log"
+	SettingDebugLogDefault = false
+)
+
+var (
+	// Defaults are the default configuration settings
+	Defaults = []config.Default{
+		{Key: SettingListen, Value: SettingListenDefault},
+		{Key: SettingMenderBackend, Value: SettingMenderBackendDefault},
+		{Key: SettingMenderUser, Value: SettingMenderUserDefault},
+		{Key: SettingMenderPass, Value: SettingMenderPassDefault},
+		{Key: SettingServerCert, Value: SettingServerCertDefault},
+		{Key: SettingServerKey, Value: SettingServerKeyDefault},
+		{Key: SettingTenantCAPem, Value: SettingTenantCAPemDefault},
+		{Key: SettingDebugLog, Value: SettingDebugLogDefault},
+	}
+)
