bc19972699a8657ad66e1860e4fce391a6069364
Marcin Chalczynski <m.chalczynski@gmail.com>
2020-07-17 09:52:04 +0200

    app/auth_provider: token management
    changelog: none
    
    Signed-off-by: Marcin Chalczynski <m.chalczynski@gmail.com>

---
+++ app/auth_provider.go
@@ -0,0 +1,40 @@
+package app
+
+import (
+	"context"
+
+	"github.com/mendersoftware/mtls-ambassador-poc/client/mender"
+)
+
+type AuthProvider interface {
+	GetToken() (string, error)
+}
+
+type authProvider struct {
+	client mender.Client
+	token  string
+	user   string
+	pass   string
+}
+
+func NewAuthProvider(client mender.Client, user, pass string) (*authProvider, error) {
+	token, err := client.Login(context.TODO(), user, pass)
+
+	switch err {
+	case nil:
+		return &authProvider{
+			client: client,
+			user:   user,
+			pass:   pass,
+			token:  token,
+		}, nil
+	case mender.ErrUnauthorized:
+		return nil, ErrUnauthorized
+	default:
+		return nil, err
+	}
+}
+
+func (ap *authProvider) GetToken() (string, error) {
+	return ap.token, nil
+}
