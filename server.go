0a1649abb57bd1f5282d56c566c1aeeaff58ed18
Marcin Chalczynski <m.chalczynski@gmail.com>
2020-07-21 10:40:12 +0200

    server: configuration/refact
    changelog: none
    
    Signed-off-by: Marcin Chalczynski <m.chalczynski@gmail.com>

--- server.go
+++ server.go
@@ -7,31 +7,25 @@ import (
 	"net/http"
 )
 
-// predefined paths - mount in container to override
-const (
-	// regular server's SSL cert
-	ServerCert = "/etc/mtls/certs/server/server.crt"
-	ServerKey  = "/etc/mtls/certs/server/server.key"
-	// tenant's CA cert, used to verify client certs in mTLS handshake
-	TenantPEM = "/etc/mtls/certs/tenant-ca/tenant.ca.pem"
-
-	// internally we're always on 8080 - override via docker/k8s
-	Port = "8080"
-)
-
 type Server struct {
-	server *http.Server
+	server   http.Server
+	certFile string
+	keyFile  string
 }
 
-func NewServer(c *Config, h http.Handler) (*Server, error) {
-	pool, err := certPool()
+func NewServer(h http.Handler,
+	srvCertFile,
+	srvKeyFile,
+	tenantCACertFile string,
+	port string) (*Server, error) {
+	pool, err := certPool(tenantCACertFile)
 	if err != nil {
 		return nil, err
 	}
 
 	// custom TLSConfig - enables client cert verification against a custom CA
 	server := http.Server{
-		Addr:    ":" + Port,
+		Addr:    ":" + port,
 		Handler: h,
 		TLSConfig: &tls.Config{
 			ClientCAs:  pool,
@@ -40,17 +34,21 @@ func NewServer(c *Config, h http.Handler) (*Server, error) {
 	}
 
 	return &Server{
-		server: &server,
+		server:   server,
+		certFile: srvCertFile,
+		keyFile:  srvKeyFile,
 	}, nil
 }
 
 func (s *Server) Run() error {
-	return s.server.ListenAndServeTLS(ServerCert, ServerKey)
+	return s.server.ListenAndServeTLS(s.certFile, s.keyFile)
 }
 
 // certPool prepares a custom cert pool with tenant's CA - to verify client certs against
-func certPool() (*x509.CertPool, error) {
-	tenantPEM, err := ioutil.ReadFile(TenantPEM)
+func certPool(tenantCACertFile string) (*x509.CertPool, error) {
+	tenantPEM, err := ioutil.ReadFile(
+		tenantCACertFile,
+	)
 	if err != nil {
 		return nil, err
 	}
